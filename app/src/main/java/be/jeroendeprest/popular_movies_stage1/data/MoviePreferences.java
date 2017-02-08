64b8b5cd04b05816223afb1a1d423a9bb424b637
Jeroen De Prest <jeroen.deprest@hotmail.com>
2017-02-08 23:41:27 +0100

    Removed API key

--- app/src/main/java/be/jeroendeprest/popular_movies_stage1/data/MoviePreferences.java
+++ app/src/main/java/be/jeroendeprest/popular_movies_stage1/data/MoviePreferences.java
@@ -5,7 +5,7 @@ package be.jeroendeprest.popular_movies_stage1.data;
  */
 
 public class MoviePreferences {
-    public static final String API_KEY = "94d3070c8f98a4612d323a6fa918b5a2";
+    public static final String API_KEY = "";
     public static final String sort_popular = "popular";
     public static final String sort_rated = "top_rated";
     public static boolean sortByPopularity = true;
